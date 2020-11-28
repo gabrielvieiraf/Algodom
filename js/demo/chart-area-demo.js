30421718d70268dbee608b96242b7197b93a9c4d
João Antônio Cardoso <joao.maker@gmail.com>
2020-11-28 16:22:23 -0300

    Live update p/ freezers adicionado

--- js/demo/chart-area-demo.js
+++ js/demo/chart-area-demo.js
@@ -117,37 +117,6 @@ var myLineChart = new Chart(ctx, {
     }
 });
 
-const REST_API_URL = "http://ec2-52-32-33-15.us-west-2.compute.amazonaws.com:5002/"
-const USER_ID = "0"
-
-function GetSensorData(user_id, sensor_id, from_index, fCallback, /* cb args  */ ) {
-    var xhttp = new XMLHttpRequest();
-    xhttp.callback = fCallback
-    xhttp.arguments = Array.prototype.slice.call(arguments, 3);
-    xhttp.onreadystatechange = function () {
-        if (this.readyState == 4 && this.status == 200) {
-            this.callback.apply(this, this.arguments)
-        }
-    };
-    xhttp.open("GET", REST_API_URL + user_id.toString() + "/sensor/" + sensor_id.toString() + "/data/from/" + from_index.toString(), true);
-    xhttp.setRequestHeader("Content-type", "application/json");
-    xhttp.send(null);
-}
-
-function GetSpendingForecastData(user_id, from_index, fCallback, /* cb args  */ ) {
-    var xhttp = new XMLHttpRequest();
-    xhttp.callback = fCallback
-    xhttp.arguments = Array.prototype.slice.call(arguments, 3);
-    xhttp.onreadystatechange = function () {
-        if (this.readyState == 4 && this.status == 200) {
-            this.callback.apply(this, this.arguments)
-        }
-    };
-    xhttp.open("GET", REST_API_URL + user_id.toString() + "/spending_forecast/data/from/" + from_index.toString(), true);
-    xhttp.setRequestHeader("Content-type", "application/json");
-    xhttp.send(null);
-}
-
 $(document).ready(function () {
 
     const timeout_ms = 100
@@ -187,11 +156,3 @@ $(document).ready(function () {
     //    }, interval_ms);
 
 });
-
-function addData(chart, label, data) {
-    chart.data.labels.push(label);
-    chart.data.datasets.forEach((dataset) => {
-        dataset.data.push(data);
-    });
-    chart.update();
-}
---
+++ js/demo/field-update.js
@@ -0,0 +1,39 @@
+
+$(document).ready(function () {
+    var last_index = 0
+
+    const interval_ms = 1000
+    setInterval(function () {
+        const sensor_ids = [0, 1]
+
+        sensor_ids.forEach(function (sensor_id, index) {
+            const sensor_id_str = (sensor_id +1).toString()
+
+            GetSensorData(USER_ID, sensor_id, 0,
+                function () {
+                    const dataframe = JSON.parse(this.responseText)
+                    //console.log(dataframe)
+                    for (datarow of dataframe) {
+                        //var date = new Date(datarow.timestamp * 1000)
+                        //const label = date.getMonth() + '/' + date.getYear()
+
+                        const temperature = datarow.temperature + ' °C'
+                        const voltage = datarow.voltage + ' V'
+                        const current = datarow.current + ' A'
+                        const status = datarow.status + ''
+
+                        updateValue("freezer_" + sensor_id_str + "_temperature", temperature)
+                        updateValue("freezer_" + sensor_id_str + "_status", status)
+                        updateValue("freezer_" + sensor_id_str + "_voltage", voltage)
+                        updateValue("freezer_" + sensor_id_str + "_current", current)
+
+                        last_index += 1
+                    }
+                }
+            )
+
+        })
+
+    }, interval_ms);
+
+});
